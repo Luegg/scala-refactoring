108857591abdaf1660cfef48adf925fb71bc75c2
Luegg <wegmaluk@gmail.com>
2013-09-30 14:21:18 +0200

    simplify extractclosure preparation

--- org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/implementations/ExtractClosure.scala
+++ org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/implementations/ExtractClosure.scala
@@ -9,38 +9,45 @@ import scala.tools.refactoring.analysis.Indexes
 abstract class ExtractClosure extends MultiStageRefactoring with TreeAnalysis with Indexes with CompilerAccess {
   import global._
 
-  case class PreparationResult(selectedExpr: Tree, owner: DefTree, potentialParameters: List[Symbol])
+  case class PreparationResult(potentialParameters: List[Symbol])
 
-  case class RefactoringParameters(closureName: String, becomesParamFilter: Symbol => Boolean)
+  case class RefactoringParameters(closureName: String, closureParameters: Symbol => Boolean)
 
-  def prepare(s: Selection): Either[PreparationError, PreparationResult] = {
-    def afterOrInSelection(p: Position) =
-      if (s.pos.start == s.pos.end)
-        p.isRange && p.start > s.pos.start
-      else
-        p.sameRange(s.pos)
-
-    def extractableTree =
-      s.root.find {
-        case t @ (_: SymTree | _: TermTree) => afterOrInSelection(t.pos)
-        case _ => false
-      }
-
-    def owner =
-      s.findSelectedWithPredicate { t =>
-        t match {
-          case _: DefDef => true
-          case _: ImplDef => true
-          case _ => false
-        }
-      } map (_.asInstanceOf[DefTree])
-
-    extractableTree.map { tree =>
-      PreparationResult(tree, owner.get, inboundLocalDependencies(s, owner.get.symbol))
-    }.toRight(PreparationError("Selection can't be extracted"))
+  def inboundDependencies(selection: Selection): List[Symbol] = {
+    for (
+      selected <- selection.selectedSymbols;
+      declaration <- index.declaration(selected) if !selection.contains(declaration) && selected.pos.isOpaqueRange
+    ) yield selected
   }
 
-  def perform(selection: Selection, selectedExpression: PreparationResult, name: RefactoringParameters): Either[RefactoringError, List[Change]] = {
+  def outboundDependencies(selection: Selection): List[Symbol] = {
+    val declarationsInTheSelection = selection.selectedSymbols filter (s => index.declaration(s).map(selection.contains) getOrElse false)
+
+    val occurencesOfSelectedDeclarations = declarationsInTheSelection flatMap (index.occurences)
+
+    occurencesOfSelectedDeclarations.filterNot(selection.contains).map(_.symbol).distinct
+  }
+
+  def prepare(s: Selection): Either[PreparationError, PreparationResult] = {
+    if (s.selectedTopLevelTrees.size > 0)
+      Right(PreparationResult(inboundDependencies(s)))
+    else
+      Left(PreparationError("No expression or statement selected."))
+  }
+
+  def perform(selection: Selection, preparation: PreparationResult, userInput: RefactoringParameters): Either[RefactoringError, List[Change]] = {
+    val closure = {
+      val params = preparation.potentialParameters.filter(userInput.closureParameters(_))
+        .map(p => q"val ${TermName(p.nameString)}: ${p.tpe}")
+      val returns = outboundDependencies(selection)
+      val returnStatement = if (returns.isEmpty) Nil else mkReturn(returns) :: Nil
+      q"""
+      def ${newTermName(userInput.closureName)}(..$params) = {
+      	..${selection.selectedTopLevelTrees}
+      	..$returnStatement
+      }
+      """
+    }
     Left(RefactoringError("not yet implemented"))
   }
 }
