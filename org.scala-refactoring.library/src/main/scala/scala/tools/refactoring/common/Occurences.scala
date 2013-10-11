8faa5fe7f31cddf6cd0e5b755d6bd9a3f43b0e25
Luegg <wegmaluk@gmail.com>
2013-10-11 14:19:12 +0200

    add occurence type member to allow overriding of occurrence conversion

--- org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/common/Occurences.scala
+++ org.scala-refactoring.library/src/main/scala/scala/tools/refactoring/common/Occurences.scala
@@ -8,6 +8,8 @@ import scala.tools.refactoring.analysis.Indexes
  */
 trait Occurrences extends Selections with CompilerAccess with Indexes {
   import global._
+  
+  type Occurrence = (Int, Int)
 
   private def termNameDefinition(selection: Selection, name: String) = {
     selection.enclosingTree.collect {
@@ -16,13 +18,13 @@ trait Occurrences extends Selections with CompilerAccess with Indexes {
     }.headOption
   }
   
-  def toOccurrence(pos: Position) =
+  def toOccurrence(pos: Position): Occurrence =
     (pos.start, pos.end - pos.start)
 
   /**
    * Returns all uses of the term name introduced by the DefTree t.
    */
-  def allOccurrences(t: DefTree) = {
+  def allOccurrences(t: DefTree): List[Occurrence] = {
     val defOccurrences = toOccurrence(t.namePosition())
     val refOccurrences = index.references(t.symbol).map {
       ref => toOccurrence(ref.pos)
@@ -35,14 +37,14 @@ trait Occurrences extends Selections with CompilerAccess with Indexes {
    * returns the occurrences of it and all its
    * references or an empty list if definition not found.
    */
-  def termNameOccurrences(selection: Selection, name: String) = {
+  def termNameOccurrences(selection: Selection, name: String): List[Occurrence] = {
     termNameDefinition(selection, name) match {
       case Some(t) => allOccurrences(t)
       case None => Nil
     }
   }
 
-  def defDefParameterOccurrences(selection: Selection, defName: String) = {
+  def defDefParameterOccurrences(selection: Selection, defName: String): List[List[Occurrence]] = {
     termNameDefinition(selection, defName) match {
       case Some(DefDef(_, _, _, params, _, _)) =>
         params.flatten.map { p => allOccurrences(p) }
